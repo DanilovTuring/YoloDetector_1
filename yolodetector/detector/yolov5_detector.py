468029af1807355b5283d7320ce083a1edaf1bb4
DanilovTuring <danilogonzalezx77@gmail.com>
2025-06-11 18:42:05 -0500

    Restructure project folders and files for better organization and maintainability

--- yolodetector/detector/yolov5_detector.py
+++ yolodetector/detector/yolov5_detector.py
@@ -6,5 +6,6 @@ from pathlib import Path
 from typing import List, Tuple, Any
 
 from yolodetector.detector.base import BaseDetector
-
+from yolodetector.config.setting import MODEL_PATH, DEVICE
+from yolodetector.utils.image_utils import preprocess_image, draw_boxes
 
