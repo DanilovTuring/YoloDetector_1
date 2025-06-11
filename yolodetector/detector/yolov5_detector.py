3083b3a509e11d06921793e83c5077caf0688975
DanilovTuring <danilogonzalezx77@gmail.com>
2025-06-11 18:33:13 -0500

    Restructure project folders and files for better organization and maintainability

---
+++ yolodetector/detector/yolov5_detector.py
@@ -0,0 +1,10 @@
+#YOLODETECTOR_1/detector/yolov5_detector.py
+
+import torch
+import cv2
+from pathlib import Path
+from typing import List, Tuple, Any
+
+from yolodetector.detector.base import BaseDetector
+
+
